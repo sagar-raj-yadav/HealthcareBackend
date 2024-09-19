9f63c15a7db64683c1e5c6c8b15475e13b5c5fe8
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2024-09-19 17:41:55 +0530

    Update serviceRoutes.js

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -61,9 +61,7 @@ router.put('/updateservice/:id', async (req, res) => {
       { new: true, runValidators: true }
     );
 
-    if (!updatedService) {
-      return res.status(404).send('Service not found');
-    }
+  
 
     res.send(updatedService);
   } catch (err) {
